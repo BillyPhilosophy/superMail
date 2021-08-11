5a449e3bbdbb03738f40f2caebd54eed57ab9f91
BillyPhilosophy <497717947@qq.com>
2021-08-11 17:57:44 +0800

    初始化项目目录

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    1233
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p>123123</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  created() {
+    console.log('123123213');
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="less" scoped>
+  p{
+    color: aqua;
+  }
 </style>
