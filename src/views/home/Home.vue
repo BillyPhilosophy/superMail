bc5138ada5c5213b45d7ad42b1cdbcef754a16d0
BillyPhilosophy <d497717947@gmail.com>
2021-08-16 22:12:06 +0800

    推荐位添加

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,26 +5,37 @@
 				购物街
 			</template>
 		</nav-bar>
+		<home-swiper :bannerList="bannerList" />
+		<home-recommend :recommendList="recommentList"/>
 	</div>
 </template>
 <script>
-import NavBar from 'components/common/navbar/NavBar'
 import *as homeNetWork from'network/home'
+import NavBar from 'components/common/navbar/NavBar'
+import HomeSwiper from './homeChild/HomeSwiper'
+import HomeRecommend from './homeChild/HomeRecommend'
 export default {
 	name: "Home",
 	data () {
-	 return {}
+	 return {
+		 bannerList:[],
+		 recommentList:[]
+	 }
 	},
 	created() {
 		homeNetWork.getHomeMultiData().then((res)=>{
 			console.log(res);
+			this.bannerList=res.data.banner.list;
+			this.recommentList=res.data.recommend.list;
 		},(err)=>{
 			console.log(err);
 		})
 	},
 	 methods:{},
 	 components:{
-		 NavBar
+		 NavBar,
+		 HomeSwiper,
+		 HomeRecommend
 	 }
 }
 </script>
---
+++ src/views/home/homeChild/HomeRecommend.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="home-recommend">
+		<div class="recommend-item" v-for="item in recommendList">
+			<a :href="item.link">
+				<img :src="item.image" alt="">
+				<div>{{item.title}}</div>
+			</a>
+		</div>
+	</div>
+</template>
+<script>
+export default {
+	name: "RecommendList",
+	props:{
+		recommendList:{
+			type:Array,
+			default(){
+				return []
+			},
+			required:true
+		}
+	},
+
+}
+</script>
+<style  scoped>
+	.home-recommend{
+		width: 100%;
+		display: flex;
+		text-align: center;
+    padding: 5px;
+    border-bottom: 10px solid #eee;
+	}
+	.recommend-item{
+		flex: 1;
+	}
+	.home-recommend img{
+		width: 70px;
+		height: 70px;
+		border-radius: 100%;
+		margin-bottom: 8px;
+	}
+</style>
